d129d65b748db75acef7b093e5d2782a72f92eb9
marcosbaiao <marcos_onlytech@hotmail.com>
2020-05-24 19:06:15 -0300

    COMMIT 25/05/2020 - 1 - Primeiro commit do repositório

---
+++ RoomBooking.Domain.Tests/Account/Specs/UserSpecTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomBooking.Domain.Account.Models;
+using RoomBooking.Domain.Account.Specs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoomBooking.Domain.Tests.Account.Specs
+{
+    [TestClass]
+    public class UserSpecTests
+    {
+        private IQueryable<User> _users;
+
+        public UserSpecTests()
+        {
+            var users = new List<User>();
+            users.Add(new User("marcosbaiao", "marcosbaiao"));
+
+            _users = users.AsQueryable();
+        }
+
+        [TestMethod]
+        [TestCategory("Account/User - Specifications")]
+        public void AuthenticateUserSpecShouldReturnOne()
+        {
+            var query = UserSpecs.AuthenticateUser("marcosbaiao", "marcosbaiao");
+            var count = _users.Where(query).Count();
+
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        [TestCategory("Account/User - Specifications")]
+        public void AuthenticateUserSpecShouldReturnZero()
+        {
+            var query = UserSpecs.AuthenticateUser("marcosbaiao222", "marcosbaiao");
+            var count = _users.Where(query).Count();
+
+            Assert.AreEqual(0, count);
+        }
+    }
+}
